ce724f5c0f19e0facf8a28a862b0fc7c53afd6e6
hennie-yun <hennie9401@gmail.com>
2025-03-19 23:13:17 +0900

    first setting

---
+++ ReadingLogBackend/src/main/java/com/example/demo/ReadingLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingLogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingLogBackendApplication.class, args);
+	}
+
+}
